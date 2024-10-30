76f33082b8894cec1d381bd00bafcda0d03f9398
tbjones1612 <tbjonesphoto@gmail.com>
2024-10-29 21:10:51 -0400

    Blank pages

--- src/app/instructor-admin/(user_man)/manage_users/page.tsx
+++ src/app/instructor-admin/(user_man)/manage_users/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import styles from "../../page.module.css";
-import Header from '../../components/Header';
-import PrevNext from '../../components/PrevNext';
-import QuestionList from '../../components/QuestionList';
+import styles from "../page.module.css";
+import Header from '../../../components/Header';
 
 const Page: React.FC = () => {
-  const pageTitle = "Module thing";
+  const pageTitle = "Manage Users";
   /* Set page title here, displayed in Header */
 
   return (
@@ -13,7 +11,7 @@ const Page: React.FC = () => {
       <Header pageTitle={pageTitle} />
       <div className="main-workspace">
         {/* Add your content within the main-workspace div */}
-        <PrevNext />
+        
       </div>
     </div>
   );
---
+++ src/app/student/exam/attempt_2/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from "../page.module.css";
+import Header from '../../../components/Header';
+
+const Page: React.FC = () => {
+  const pageTitle = "Exam - Attempt 2";
+  /* Set page title here, displayed in Header */
+
+  return (
+    <div className={styles.Container}>
+      <Header pageTitle={pageTitle} />
+      <div className="main-workspace">
+        {/* Add your content within the main-workspace div */}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Page;
+
