0c0975d2aad0122ef164e2d480f262005422cbf0
trinity-thiele <trin.thiele@gmail.com>
2024-11-06 12:10:18 -0500

    Exam Sliders

--- src/app/components/StudentCourseHome.tsx
+++ src/app/components/StudentCourseHome.tsx
@@ -2,14 +2,22 @@
 
 import React, { useState } from 'react';
 import './component-style.css';
+import ExamSlide from './ExamSlide';
 
 const StudentCourseHome = () => {
 
     return (
         <div className='student-course-home-body'>
             <div className="menu-panel upcoming-exams">
-            <h3>Upcoming Exams</h3><br />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore expedita consequuntur quia alias vitae sed molestias laboriosam vel facilis recusandae quos assumenda eaque atque reprehenderit harum rem, at et ratione?</p>
+                <h3>Upcoming Exams</h3><br />
+                <div className="exam-slide-box">
+                    <ExamSlide />
+                    <ExamSlide />
+                    <ExamSlide />
+                    <ExamSlide />
+                    <ExamSlide />
+                    <ExamSlide />
+                </div>
             </div>
             <div className="menu-panel course-info">
                 <h3>Course Information</h3><br />
