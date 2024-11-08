c908f547a16d77253bc55dd9209c9e208133ad66
Jayden Asbie <jaydenasbie@gmail.com>
2024-11-08 10:47:42 -0500

    added student dashboard

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from "./page.module.css";
 import Header from '../../components/Header';
+import PrevNext from '@/app/components/PrevNext';
+import CourseList from '@/app/components/CourseList';
 
 const Page: React.FC = () => {
   const pageTitle = "Dashboard";
@@ -11,7 +13,7 @@ const Page: React.FC = () => {
       <Header pageTitle={pageTitle} />
       <div className="main-workspace">
         {/* Add your content within the main-workspace div */}
-        
+        <CourseList></CourseList>
       </div>
     </div>
   );
